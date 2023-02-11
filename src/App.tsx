fb39fb0f4a05ae07b1e1e2e93c683387c944e2e5
i.shablovskiy <i.shablovskiy@iotans.by>
2023-02-11 23:00:49 +0300

    edit Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from './components/Card';
+import { CardVariant } from './components/Card/Card';
 
 const App = () => {
   return (
     <div>
-      <Card height="200px" width="200px">
+      <Card variant={CardVariant.primary} height="200px" width="200px">
         <button>Кнопка</button>
+        <p>Some children text</p>
       </Card>
     </div>
   );
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { FC } from 'react';
+
+export enum CardVariant {
+  outlined = 'outlined',
+  primary = 'primary',
+}
 
 interface CardProps {
   width?: string;
   height?: string;
+  variant: CardVariant;
   children?: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ width, height, children }) => {
-  return <div style={{ width, height, border: '1px solid gray' }}>{children}</div>;
+const Card: FC<CardProps> = ({ width, height, variant, children }) => {
+  return (
+    <div
+      style={{
+        width,
+        height,
+        border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+        background: variant === CardVariant.primary ? 'lightgray' : '',
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Card;
