1001739d0c5b19e5c288c3ac89d29574acaa8e05
i.shablovskiy <i.shablovskiy@iotans.by>
2023-02-12 01:45:28 +0300

    add users components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import Card from './components/Card';
 import { CardVariant } from './components/Card/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
+
+const users: IUser[] = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+    },
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    email: 'Shanna@melissa.tv',
+    address: {
+      street: 'Victor Plains',
+      city: 'Wisokyburgh',
+      zipcode: '90566-7771',
+    },
+  },
+];
 
 const App = () => {
   return (
@@ -9,6 +34,7 @@ const App = () => {
         <button>Кнопка</button>
         <p>Some children text</p>
       </Card>
+      <UserList users={users} />
     </div>
   );
 };
